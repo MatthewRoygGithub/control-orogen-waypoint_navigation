01d26e8bf9b3702ef9f2a4640d98b5eb1ea104ba
filip-jan <jan.filip2@gmail.com>
2016-10-13 14:37:56 +0200

    Configuration of library reimplemented: pathTrackerConfig struct moved to the component side, added .hpp with pathTrackerConfig struct  and .yaml files

---
+++ waypoint_navigationTypes.hpp
@@ -0,0 +1,34 @@
+#ifndef WAYPOINT_NAVIGATION_TYPE_HPP
+#define WAYPOINT_NAVIGATION_TYPE_HPP
+
+namespace waypoint_navigation {
+
+    /** Configuration **/
+	struct pathTrackerConfig
+	{	
+		// Paramater							Unit
+		//-----------------------------------------------------------------------
+		// Minimum radius of robot's Ackermann turn			 (m)
+		double minTurnRadius;
+
+		// Robot's translational velocity during Ackermann turns	 (m/s)
+		double translationalVelocity;
+		
+		// Robot's rotational velocity during point turns		 (rad/s)
+		double rotationalVelocity;
+
+		// Safety corridor (1/2 width) arround the nominal trajectory	 (m)
+		double corridor;		
+
+		// Maximum allowed disalignment from target heading in the final pose (rad)
+		double maxDisalignment;	
+		
+		// Distance to the lookahead point of the Pure Pursuit Algorithm (m)
+		double lookaheadDistance;	
+    };
+}
+#endif
+
+
+
+
